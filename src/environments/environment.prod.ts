febbe91f63b755b4f4263c1acd6ee3871eb05feb
Max Aldunate <maxaldunate@gmail.com>
2018-09-17 16:59:48 +0200

    env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
     production: true,
 
-    region: 'us-east-1',
+    region: '',
 
-    identityPoolId: 'us-east-1:fbe0340f-9ffc-4449-a935-bb6a6661fd53',
-    userPoolId: 'us-east-1_PGSbCVZ7S',
-    clientId: 'hh5ibv67so0qukt55c5ulaltk',
+    identityPoolId: '',
+    userPoolId: '',
+    clientId: '',
 
-    rekognitionBucket: 'rekognition-pics',
-    albumName: "usercontent",
-    bucketRegion: 'us-east-1',
+    rekognitionBucket: '',
+    albumName: "",
+    bucketRegion: '',
 
-    ddbTableName: 'LoginTrail',
+    ddbTableName: '',
 
     cognito_idp_endpoint: '',
     cognito_identity_endpoint: '',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,22 @@
 export const environment = {
     production: false,
 
-    region: 'us-east-1',
+    region: 'eu-west-1',
 
-    identityPoolId: 'us-east-1:fbe0340f-9ffc-4449-a935-bb6a6661fd53',
-    userPoolId: 'us-east-1_PGSbCVZ7S',
-    clientId: 'hh5ibv67so0qukt55c5ulaltk',
+    identityPoolId: 'eu-west-1:28989c83-bbe3-4796-b3fa-43cf0db4026c',
+    userPoolId: 'eu-west-1_WipDT4VRl',
+    clientId: '18s6c1bo10o70jnlik7082ofjc',
 
-    rekognitionBucket: 'rekognition-pics',
-    albumName: "usercontent",
-    bucketRegion: 'us-east-1',
+    rekognitionBucket: '',
+    albumName: "",
+    bucketRegion: '',
 
-    ddbTableName: 'LoginTrail',
+    ddbTableName: 'LaguineuUserSession',
 
     cognito_idp_endpoint: '',
     cognito_identity_endpoint: '',
     sts_endpoint: '',
-    dynamodb_endpoint: '',
+    dynamodb_endpoint: 'arn:aws:dynamodb:eu-west-1:591364439420:table/LaguineuUserSession',
     s3_endpoint: ''
-
 };
 
